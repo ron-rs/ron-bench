8eeaa034e38fdb45bae30356cfb7feb5d5c3fec7
Thomas Schaller <me@torkleyy.com>
2021-10-16 14:02:08 +0200

    Add json to comparison

--- src/bin/convert-json.rs
+++ src/bin/convert-json.rs
@@ -1,9 +1,10 @@
-use std::fs::File;
 use ron::ser::to_writer;
 use serde_json::from_reader as json_from_reader;
+use std::fs::File;
 
 // Convert .json to .ron
 fn main() {
-    let value: serde_json::Value = json_from_reader(File::open("../../data/canada.json").unwrap()).unwrap();
+    let value: serde_json::Value =
+        json_from_reader(File::open("../../data/canada.json").unwrap()).unwrap();
     to_writer(File::create("../../data/canada.ron").unwrap(), &value).unwrap();
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,12 @@
+#![allow(dead_code)]
+
+use criterion::{criterion_group, BenchmarkId, Criterion, PlottingBackend};
 use std::fs::read_to_string;
-use criterion::{criterion_group, Criterion, BenchmarkId, PlottingBackend};
+use std::path::{Path, PathBuf};
+
+fn json_de(content: &str) -> impl Sized {
+    serde_json::from_str::<ron::Value>(content)
+}
 
 fn ron_de_legacy(content: &str) -> impl Sized {
     ron::from_str::<ron::Value>(content)
@@ -9,14 +16,27 @@ fn ron_de_reboot(content: &str) -> impl Sized {
     ron_reboot::from_str::<ron::Value>(content)
 }
 
+fn with_extension(path: impl AsRef<Path>, ext: &str) -> PathBuf {
+    path.as_ref().with_extension(ext)
+}
+
 fn bench_serde_de(c: &mut Criterion) {
     let mut group = c.benchmark_group("Serde Deserialization");
-    for i in ["data/canada.ron"].iter() {
-        let content = read_to_string(i).unwrap();
-        group.bench_with_input(BenchmarkId::new("ron", i), &content,
-                               |b, i| b.iter(|| ron_de_legacy(i)));
-        group.bench_with_input(BenchmarkId::new("ron-reboot", i), &content,
-                               |b, i| b.iter(|| ron_de_reboot(i)));
+    for file in ["data/canada"].iter() {
+        let content_json = read_to_string(&with_extension(file, "json")).unwrap();
+        let content = read_to_string(&with_extension(file, "json")).unwrap();
+        group.bench_with_input(BenchmarkId::new("json", file), &content_json, |b, i| {
+            b.iter(|| json_de(i))
+        });
+        group.bench_with_input(BenchmarkId::new("ron", file), &content, |b, i| {
+            b.iter(|| ron_de_legacy(i))
+        });
+        /*
+        TODO: infinite loop bug...
+        group.bench_with_input(BenchmarkId::new("ron-reboot", i), &content, |b, i| {
+            b.iter(|| ron_de_reboot(i))
+        });
+         */
     }
     group.finish();
 }
@@ -27,5 +47,8 @@ criterion_group!(benches, bench_serde_de);
 fn main() {
     benches();
 
-    Criterion::default().configure_from_args().plotting_backend(PlottingBackend::Plotters).final_summary();
+    Criterion::default()
+        .configure_from_args()
+        .plotting_backend(PlottingBackend::Plotters)
+        .final_summary();
 }
